850df3547d829b8968c394ea06b409e2107423cf
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-04-02 01:49:17 -0700

    Route components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Box } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 import { styles } from './styles'
 
@@ -9,10 +10,10 @@ const Navbar = ({ totalItems }) => {
     <>
         <AppBar position='fixed' sx={styles.appBar} color="inherit">
             <Toolbar>
-                <Box sx={styles.image}>
+                <Box sx={styles.image} component={Link} to='/'>
                     <img src='Critterspellpng.png' alt="Critterspell" height="55px" />
                 </Box>
-                <IconButton aria-label='Show cart items' color='inherit'>
+                <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
                     <Badge badgeContent={totalItems} color='secondary'>
                         <ShoppingCartIcon />
                     </Badge>
