f80ebc88fb04bd26f925605627459e5cee2d8693
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-10 13:47:16 -0700

    Add basic styles to content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { Typography, Box } from '@mui/material';
-import { Offset } from './styles'
+import { Typography, Box, Container } from '@mui/material';
+import { Styles, Offset } from './styles'
+import { useTheme } from "@mui/material/styles"; 
 
 import Products from '../Products/Products';
 
 function Home({ products, onAddToCart }) {
+    const theme = useTheme();
+    const styles = Styles(theme);
+
   return (
     <>
         <Offset />
-        <Box>
-            <Typography>Welcome to Critterspell</Typography>
+        <Container sx={styles.container}>
+            <Box>
+                <Typography sx={styles.title} variant='h1'>Welcome to Critterspell</Typography>
 
-            <Typography>Welcome to our charming world of custom name art! Our delightful animal-inspired
-                letters are the perfect way to add a touch of whimsy to any child's room. Whether you're
-                looking for a unique baby shower gift, a special birthday present, or a fun way to decorate your own child's space,
-                our custom name art is sure to delight. Each letter is carefully crafted with love and
-                attention to detail, resulting in a beautiful and personalized work of art that will be cherished for years to come.
-                We invite you to browse our collection and discover the perfect combination of animals to spell out your child's name.
-            </Typography>
-        </Box>
-        <Box>
-            <Products products={products} onAddToCart={onAddToCart}/>
-        </Box>
+                <Typography sx={styles.content} variant='body1'>Welcome to our charming world of custom name art! Our delightful animal-inspired
+                    letters are the perfect way to add a touch of whimsy to any child's room. Whether you're
+                    looking for a unique baby shower gift, a special birthday present, or a fun way to decorate your own child's space,
+                    our custom name art is sure to delight. Each letter is carefully crafted with love and
+                    attention to detail, resulting in a beautiful and personalized work of art that will be cherished for years to come.
+                    We invite you to browse our collection and discover the perfect combination of animals to spell out your child's name.
+                </Typography>
+            </Box>
+            <Box>
+                <Products products={products} onAddToCart={onAddToCart}/>
+            </Box>
+        </Container>
     </>
   )
 }
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,3 +1,23 @@
-import { styled } from "@mui/material/styles"
+import { styled } from "@mui/material/styles" 
 
 export const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+
+export const Styles = (theme) => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    content: {
+        margin: '10% 20% 0',
+        textAlign: 'center'
+    },
+    title: {
+        marginTop: '5%',
+        textAlign: 'center',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '65px'
+        }
+    }
+})
