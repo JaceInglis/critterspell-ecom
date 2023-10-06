8bfa0a40e4be0ec904d13129a2b6795c15e53bfa
Jace <62110248+JaceInglis@users.noreply.github.com>
2023-10-05 22:51:03 -0700

    other colors

--- src/components/TestName/TestName.jsx
+++ src/components/TestName/TestName.jsx
@@ -15,21 +15,30 @@ import React, { useState, useEffect } from "react";
 const letters = {
   a: "/letters/Letters_small_A.png",
   b: {
-    0: "/letters/Letters_small_B-Purple.png",
-    1: "/letters/Letters_small_B.png",
+    0: { name: "Purple", src: "/letters/Letters_small_B-Purple.png" },
+    1: { name: "Pink", src: "/letters/Letters_small_B.png" },
   },
   c: "/letters/Letters_small_C.png",
   d: "/letters/Letters_small_D.png",
-  e: "/letters/Letters_small_E.png",
+  e: {
+    0: { name: "Brown", src: "/letters/Letters_small_E-Brown.png" },
+    1: { name: "Yellow", src: "/letters/Letters_small_E.png" },
+  },
   f: "/letters/Letters_small_F.png",
   g: "/letters/Letters_small_G.png",
   h: "/letters/Letters_small_H.png",
-  i: "/letters/Letters_small_I.png",
+  i: {
+    0: { name: "Brown", src: "/letters/Letters_small_I-Brown.png" },
+    1: { name: "Green", src: "/letters/Letters_small_I.png" },
+  },
   j: "/letters/Letters_small_J.png",
   k: "/letters/Letters_small_K.png",
   l: "/letters/Letters_small_L.png",
   m: "/letters/Letters_small_M.png",
-  n: "/letters/Letters_small_N.png",
+  n: {
+    0: { name: "Brown", src: "/letters/Letters_small_N-Brown.png" },
+    1: { name: "Blue", src: "/letters/Letters_small_N.png" },
+  },
   o: "/letters/Letters_small_O.png",
   p: "/letters/Letters_small_P.png",
   q: "/letters/Letters_small_Q.png",
@@ -47,15 +56,18 @@ const letters = {
 
 const TestName = () => {
   const [name, setName] = useState("Name");
-  const [nameConfig, setNameConfig] = useState({});
+  const [nameConfig, setNameConfig] = useState({
+    n: { 0: { color: 0 } },
+    e: { 3: { color: 0 } },
+  });
 
-  const theme = useTheme()
+  const theme = useTheme();
 
   const handleNameChange = (event) => {
     const newName = event.target.value;
     setName(newName);
     newName.split("").forEach((letter, index) => {
-      if (letter === "b") {
+      if (letter === "b" || letter === "n" || letter === "e" || letter === "i") {
         handleAddConfig(letter, index);
       }
     });
@@ -84,7 +96,13 @@ const TestName = () => {
   }, [nameConfig]);
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" width="100%" my={theme.spacing(8)}>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      width="100%"
+      my={theme.spacing(8)}
+    >
       <Typography variant="h1" gutterBottom>
         See how your childs name would look
       </Typography>
@@ -101,7 +119,7 @@ const TestName = () => {
           .map((letter, index) => {
             if (letter === " ") {
               return <Box minWidthidth={50} width={50} key={index} />;
-            } else if (letter === "b") {
+            } else if (letter === "b" || letter === "n" || letter === "e" || letter === "i") {
               return (
                 <Box
                   key={index}
@@ -110,7 +128,7 @@ const TestName = () => {
                     position: "relative",
                     display: "flex",
                     flexDirection: "column",
-                    alignItems: 'center'
+                    alignItems: "center",
                   }}
                 >
                   <FormControl
@@ -120,19 +138,18 @@ const TestName = () => {
                       marginBottom: "-15px",
                       position: "absolute",
                       bottom: 0,
-                      
                       width: "60%",
                     }}
                   >
                     <InputLabel id={`select-label-${index}`}>Color</InputLabel>
                     <Select
                       labelId={`select-label-${index}`}
-                      value={nameConfig[letter][index]["color"]}
+                      value={nameConfig[letter][index].color}
                       label="Color"
                       onChange={handleChangeColor(letter, index)}
                     >
-                      <MenuItem value={0}>Purple</MenuItem>
-                      <MenuItem value={1}>Pink</MenuItem>
+                      <MenuItem value={0}>{letters[letter][0].name}</MenuItem>
+                      <MenuItem value={1}>{letters[letter][1].name}</MenuItem>
                     </Select>
                   </FormControl>
                   <img
@@ -140,7 +157,7 @@ const TestName = () => {
                       maxWidth: "210px",
                       maxHeight: "200px",
                     }}
-                    src={letters[letter][nameConfig[letter][index]["color"]]}
+                    src={letters[letter][nameConfig[letter][index].color].src}
                     alt={letter}
                   />
                 </Box>
