84756fa4f0c578a9ac976f280b0cf834a3e4943e
Jace <62110248+JaceInglis@users.noreply.github.com>
2024-01-01 17:46:14 -0800

    disable console.logs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { hydrate, render } from "react-dom";
 
 import App from "./App";
 
+console.log = () => {};
+console.warn = () => {};
+console.debug = () => {};
+
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
